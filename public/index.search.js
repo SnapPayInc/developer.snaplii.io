70c6a2d076d1cec9b1226909b38bcc90ae60e51d
Ivan <ivan@macs-MacBook-Pro.local>
2023-04-13 14:21:14 -0400

    init

---
+++ public/index.search.js
@@ -0,0 +1,30 @@
+var relearn_search_index = [
+  {
+    "content": "aaa bbb ",
+    "description": "",
+    "tags": null,
+    "title": "Demo Index",
+    "uri": "/demo/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Snaplii Doc",
+    "uri": "/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
